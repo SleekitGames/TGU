637b60af69ab8c813aa45ed5d382e9464731bea4
renegaderich <richardpasse@hotmail.com>
2018-01-20 20:59:26 +0000

    Edge scrolling

--- Source/TGU/TGUGameMode.cpp
+++ Source/TGU/TGUGameMode.cpp
@@ -7,7 +7,7 @@
 ATGUGameMode::ATGUGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Dynamic/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
